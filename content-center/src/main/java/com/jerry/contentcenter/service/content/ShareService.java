82b6744b5076c6de945fe2c3f449abea1c6938f7
JerryTan <thrforever@126.com>
2020-12-10 17:42:10 +0800

    5-6 为内容中心引入服务发现

--- content-center/src/main/java/com/jerry/contentcenter/service/content/ShareService.java
+++ content-center/src/main/java/com/jerry/contentcenter/service/content/ShareService.java
@@ -5,10 +5,15 @@ import com.jerry.contentcenter.domain.content.ShareDTO;
 import com.jerry.contentcenter.domain.dto.user.UserDTO;
 import com.jerry.contentcenter.domain.entity.content.Share;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA
@@ -19,18 +24,29 @@ import org.springframework.web.client.RestTemplate;
  */
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
 public class ShareService {
 
     private final ShareMapper shareMapper;
     private final RestTemplate restTemplate;
+    private final DiscoveryClient discoveryClient;
 
     public ShareDTO findById(Integer id) {
         Share share = shareMapper.selectByPrimaryKey(id);
         Integer userId = share.getUserId();
 
+        // 获取用户中心所有实例信息
+        List<ServiceInstance> instanceList = discoveryClient.getInstances("user-center");
+        // 拿到服务地址
+        String targetUrl = instanceList.stream()
+                .map(instance -> instance.getUri() + "/users/{id}")
+                // 这里先返回第1个
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("当前没有实例"));
+        log.info("请求目标地址：{}", targetUrl);
+
         // 远程调用
-        UserDTO userDTO = restTemplate.getForObject("http://localhost:8080/users/{id}",
-                UserDTO.class, userId);
+        UserDTO userDTO = restTemplate.getForObject(targetUrl, UserDTO.class, userId);
         // 消息装配
         ShareDTO shareDTO = new ShareDTO();
         BeanUtils.copyProperties(share, shareDTO);

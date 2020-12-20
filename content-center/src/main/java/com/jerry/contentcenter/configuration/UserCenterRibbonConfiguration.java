27f2013a58b5fa2586b20433e7b1180a6f932039
JerryTan <thrforever@126.com>
2020-12-20 16:11:18 +0800

    6-8 细粒度配置自定义03-配置属性方式

--- content-center/src/main/java/com/jerry/contentcenter/configuration/UserCenterRibbonConfiguration.java
+++ content-center/src/main/java/com/jerry/contentcenter/configuration/UserCenterRibbonConfiguration.java
@@ -1,9 +1,5 @@
 package com.jerry.contentcenter.configuration;
 
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.context.annotation.Configuration;
-import ribbonconfiguration.RibbonConfiguration;
-
 /**
  * Created with IntelliJ IDEA
  * User: Jerry
@@ -11,7 +7,7 @@ import ribbonconfiguration.RibbonConfiguration;
  * Time: 14:47
  * Description:
  */
-@Configuration
-@RibbonClient(name = "user-center", configuration = RibbonConfiguration.class)
+//@Configuration
+//@RibbonClient(name = "user-center", configuration = RibbonConfiguration.class)
 public class UserCenterRibbonConfiguration {
 }

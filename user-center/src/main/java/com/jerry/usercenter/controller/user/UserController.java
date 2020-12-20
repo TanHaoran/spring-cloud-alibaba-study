8f5a07fa67968a17a8b6b13db89481ea6f2350af
JerryTan <thrforever@126.com>
2020-12-20 14:45:17 +0800

    6-5 Ribbon内置的负载均衡规则

--- user-center/src/main/java/com/jerry/usercenter/controller/user/UserController.java
+++ user-center/src/main/java/com/jerry/usercenter/controller/user/UserController.java
@@ -3,6 +3,7 @@ package com.jerry.usercenter.controller.user;
 import com.jerry.usercenter.domain.entity.user.User;
 import com.jerry.usercenter.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,12 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping("/{id}")
     public User findById(@PathVariable Integer id) {
+        log.info("UserController.findById 请求了");
         return userService.findById(id);
     }
 

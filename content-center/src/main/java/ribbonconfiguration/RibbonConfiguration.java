2718180ae9732ff3dfee15eb426e5753b396dbde
JerryTan <thrforever@126.com>
2020-12-20 16:22:56 +0800

    6-12 支持的配置项

--- content-center/src/main/java/ribbonconfiguration/RibbonConfiguration.java
+++ content-center/src/main/java/ribbonconfiguration/RibbonConfiguration.java
@@ -1,6 +1,8 @@
 package ribbonconfiguration;
 
+import com.netflix.loadbalancer.IPing;
 import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.PingUrl;
 import com.netflix.loadbalancer.RandomRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +20,11 @@ public class RibbonConfiguration {
     @Bean
     public IRule ribbonRule() {
         return new RandomRule();
+    }
+
+    @Bean
+    public IPing ping() {
+        return new PingUrl();
     }
 
 }

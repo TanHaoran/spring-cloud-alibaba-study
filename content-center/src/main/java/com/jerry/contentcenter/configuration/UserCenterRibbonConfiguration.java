bdd75272d4876eefd6276cac759a39612dc9f14a
JerryTan <thrforever@126.com>
2020-12-20 16:15:26 +0800

    6-11 全局配置

--- content-center/src/main/java/com/jerry/contentcenter/configuration/UserCenterRibbonConfiguration.java
+++ content-center/src/main/java/com/jerry/contentcenter/configuration/UserCenterRibbonConfiguration.java
@@ -1,5 +1,9 @@
 package com.jerry.contentcenter.configuration;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
+import org.springframework.context.annotation.Configuration;
+import ribbonconfiguration.RibbonConfiguration;
+
 /**
  * Created with IntelliJ IDEA
  * User: Jerry
@@ -7,7 +11,7 @@ package com.jerry.contentcenter.configuration;
  * Time: 14:47
  * Description:
  */
-//@Configuration
-//@RibbonClient(name = "user-center", configuration = RibbonConfiguration.class)
+@Configuration
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 public class UserCenterRibbonConfiguration {
 }

d543cee1df881ab318e85b66c289c5756a721765
JerryTan <thrforever@126.com>
2020-12-20 16:35:09 +0800

    6-13 饥饿加载

--- content-center/src/main/java/ribbonconfiguration/RibbonConfiguration.java
+++ content-center/src/main/java/ribbonconfiguration/RibbonConfiguration.java
@@ -1,8 +1,6 @@
 package ribbonconfiguration;
 
-import com.netflix.loadbalancer.IPing;
 import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.PingUrl;
 import com.netflix.loadbalancer.RandomRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,11 +18,6 @@ public class RibbonConfiguration {
     @Bean
     public IRule ribbonRule() {
         return new RandomRule();
-    }
-
-    @Bean
-    public IPing ping() {
-        return new PingUrl();
     }
 
 }

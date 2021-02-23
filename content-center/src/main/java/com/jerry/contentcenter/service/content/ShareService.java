c3f85025bef37103ea68009ea3b9834ac1aad10b
tanhaoran <thrforever@126.com>
2021-02-23 17:48:12 +0800

    还原为 FeignClient

--- content-center/src/main/java/com/jerry/contentcenter/service/content/ShareService.java
+++ content-center/src/main/java/com/jerry/contentcenter/service/content/ShareService.java
@@ -9,14 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Collectors;
 
 /**
  * Created with IntelliJ IDEA
@@ -32,24 +25,14 @@ public class ShareService {
 
     private final ShareMapper shareMapper;
 
-    private final DiscoveryClient discoveryClient;
-
-    private final RestTemplate restTemplate;
+    private final UserCenterFeignClient userCenterFeignClient;
 
     public ShareDTO findById(Integer id) {
         Share share = shareMapper.selectByPrimaryKey(id);
         Integer userId = share.getUserId();
 
         // 远程调用
-        List<ServiceInstance> instanceList = discoveryClient.getInstances("user-center");
-        // 拿到服务地址
-        String targetUrl = instanceList.stream()
-                .map(instance -> instance.getUri() + "/users/{id}")
-                // 这里先返回第1个
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("当前没有实例"));
-
-        UserDTO userDTO = restTemplate.getForObject(targetUrl, UserDTO.class, userId);
+        UserDTO userDTO = userCenterFeignClient.findById(userId);
 
         // 消息装配
         ShareDTO shareDTO = new ShareDTO();
